87c5ad22b7e81237b625efd96c96206c37af5dba
ainshaikh <ainsaikhx7@gmail.com>
2022-06-19 21:51:37 +0530

    camera app completed

--- Browser/camera/db.js
+++ Browser/camera/db.js
@@ -1,23 +1,20 @@
-// Connect(open) to  database
-
+//connect to database
 let db;
-let openRequest = indexedDB.open('myDatabse');
-openRequest.addEventListener('success',()=>{
+let openRequest = indexedDB.open('myDatabase');
+let a = 123;
+openRequest.addEventListener('success', () => {
     console.log('db connected');
     db = openRequest.result;
-});
+})
 
-
-openRequest.addEventListener('upgradeneeded',()=>{
-    console.log('db upgrade or initialised db');
+openRequest.addEventListener("upgradeneeded", () => {
+    console.log("db upgraded OR initalised db ");
     db = openRequest.result;
 
-
-    db.createObjectStore('video',{keyPath: "id"});
-    db.createObjectStore('Image',{keypath:"id"});
-
+    db.createObjectStore('video', { keyPath: "id" });
+    db.createObjectStore("image", { keyPath: "id" });
 });
 
-openRequest.addEventListener('error',()=>{
-    console.log('db error');
+openRequest.addEventListener("error", () => {
+    console.log("db error");
 });
