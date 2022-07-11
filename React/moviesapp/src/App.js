c6aa3dd840c3416cf6ac1e93d62b705ae9924244
ainshaikh <ainsaikhx7@gmail.com>
2022-07-11 10:22:41 +0530

    :construction:completed banner and navbar

---
+++ React/moviesapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+import Banner from './component/Banner';
+
+
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <Banner/>
+
+    </>
+   
+  );
+}
+
+export default App;
