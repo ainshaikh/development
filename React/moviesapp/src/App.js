045981aded3a7bc81bc50cbe96c1a440930cdd9d
ainshaikh <ainsaikhx7@gmail.com>
2022-07-12 13:39:49 +0530

    :construction:partially completed

--- React/moviesapp/src/App.js
+++ React/moviesapp/src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './component/Navbar';
-import Banner from './component/Banner';
-
-
+import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
+import List from './Components/List';
 function App() {
   return (
     <>
-     <Navbar/>
-     <Banner/>
-
+      <Navbar />
+      <Banner />
+      <List/>
     </>
-   
   );
 }
 
