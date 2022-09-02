00db664d11e4d195d165d99461e0dbbf0453fcc8
ainshaikh <ainsaikhx7@gmail.com>
2022-09-02 10:20:16 +0530

    :fire:read conditional branching 'if'

---
+++ PRACTICE/conditionalbranching.js
@@ -0,0 +1,124 @@
+let year = prompt("In which year India got its independence");
+if(year==1947){}
+undefined
+let year = prompt("In which year India got its independence");
+if(year==1947)alert{
+    "You are correct"
+};
+VM485:2 Uncaught SyntaxError: Unexpected token '{'
+let year = prompt("In which year India got its independence");
+if(year==1947){
+    alert("You are correct");
+};
+undefined
+let year = prompt("In which year India got its independence");
+if(year==1947){
+    alert("You are correct");
+};
+undefined
+let year = prompt("In which year India got its independence");
+if(year==1947){
+    alert("You are correct");
+};
+else{alert("you are wrong");
+    };
+VM712:5 Uncaught SyntaxError: Unexpected token 'else'
+let year = prompt("In which year India got its independence");
+if(year==1947){
+    alert("You are correct");
+}
+else{alert("you are wrong");
+    };
+undefined
+// ternary operator or question mark
+undefined
+let accessAllowed;
+let age = prompt("How old are you");
+if(age>18) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
+}
+alert(accessAllowed);
+
+    
+undefined
+let entryAllowed;
+let age = prompt("How old are you?");
+if(age>25){
+    entryAllowed = You are welcome;
+} else {
+    entryAllowed = Get lost 
+};
+alert(entryAllowed);
+VM1652:4 Uncaught SyntaxError: Unexpected identifier
+let entryAllowed;
+let age = prompt("How old are you?");
+if(age>25){
+    entryAllowed = You are welcome
+} else {
+    entryAllowed = Get lost 
+}
+alert(entryAllowed);
+VM1698:4 Uncaught SyntaxError: Unexpected identifier
+let entryAllowed;
+let age = prompt("How old are you?")
+if(age>25){
+    entryAllowed = You are welcome
+} else {
+    entryAllowed = Get lost 
+}
+alert(entryAllowed);
+VM1706:4 Uncaught SyntaxError: Unexpected identifier
+let entryAllowed;
+let sage = prompt("How old are you?");
+if(sage>25){
+    entryAllowed = You are welcome
+} else {
+    entryAllowed = Get lost 
+}
+alert(entryAllowed);
+VM1764:4 Uncaught SyntaxError: Unexpected identifier
+let entryAllowed;
+let age = prompt("How old are you?");
+if(age>25){
+    entryAllowed = true;
+} else {
+    entryAllowed = false;
+}
+alert(entryAllowed);
+undefined
+let accessAllowed = age>18 ? true : false;
+undefined
+let accessAllowed = (age > 18) ? true : false;
+alert(accessAllowed);
+undefined
+
+let age = prompt('age?', 18);
+
+
+let message = (age <3 ) ? 'Hi baby!' :
+    (age<18) ? "hello" :
+    (age <100 ) ? "greetings" :
+    "what an ususal age";
+alert(message);
+undefined
+let age = prompt('age?', 18);
+let message = (age <3 ) ? 'Hi baby!' :
+    (age<18) ? "hello" :
+    (age <100 ) ? "greetings" :
+    "what an ususal age";
+alert(message);
+undefined
+// non traditional use of ?
+undefined
+let company = prompt("which company created Javascript");
+(company=="Netscape") ?
+    alert("right") : alert("wrong");
+undefined
+if("0") {
+    alert("Hello");
+};
+undefined
+alert("0");
+undefined
