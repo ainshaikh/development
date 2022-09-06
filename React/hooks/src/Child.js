4f1642cd28ee97d06c7721fb402c80b349c548af
MOHD AIN <98968856+ainshaikh@users.noreply.github.com>
2022-09-06 10:00:02 +0530

    Delete React directory

--- React/hooks/src/Child.js
+++
@@ -1,5 +0,0 @@
-import React, {useContext} from "react";
-function Child({theme}){
-    return <div className={theme?"dark":"light"}>child</div>;
-}
-export default Child;
--- React/hooks/src/Components/Context.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Context = React.createContext("Good Evening");
-export default Context;
